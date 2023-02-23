37e8e430b5e7ce1bf0d48069c6d29311631ac31c
EQuinteros11 <2508722019@mail.utec.edu.sv>
2023-02-22 22:19:19 -0600

    Ultimos Cambios

--- ParcialIEjercico2/app/src/main/java/utec/edu/sv/parcialiejercico2/MainActivity.java
+++ ParcialIEjercico2/app/src/main/java/utec/edu/sv/parcialiejercico2/MainActivity.java
@@ -26,6 +26,34 @@ TextView tvMostrar;
         peso = Double.parseDouble(edtPeso.getText().toString());
         esta= Double.parseDouble(edtEsta.getText().toString());
         IMC = ((peso)/(esta*esta));
-        
+        if (IMC<10.5){
+            tvMostrar.setText("Criticamente bajo de peso");
+        }
+        else
+            if (IMC<15.9){
+                tvMostrar.setText("Severamente bajo de peso");
+            }
+            else
+                if (IMC<18.5){
+                    tvMostrar.setText("Bajo de Peso");
+                }
+                else
+                    if (IMC < 25){
+                        tvMostrar.setText("Normal (Peso Saludable)");
+                    }
+                    else
+                        if(IMC<30){
+                            tvMostrar.setText("Sobrepeso");
+                    }
+                        else
+                            if (IMC<35){
+                                tvMostrar.setText("Obesidad Clase 1 - Moderadamente Obeso");
+                            }
+                            else
+                                if (IMC<40){
+                                    tvMostrar.setText("Obesidad clase 2 - Severamente Obeso");
+                            }
+                                else
+                                    tvMostrar.setText("Obesidad Clase 3 - Criticamente Obeso");
     }
 }
